aff02e26d2aa846171589b56871663c21042356b
Grant Murray <grant.murray@yum.com>
2020-05-12 18:30:42 -0500

    Caching of creds path functioning properly - needs tests around it in case it ever explodes

--- src/snowmobile/snowcreds.py
+++ src/snowmobile/snowcreds.py
@@ -1,5 +1,7 @@
+
 import os
 import json
+from fcache.cache import FileCache
 
 
 class Credentials:
@@ -15,9 +17,98 @@ class Credentials:
             use first set of credentials in the file if no argument is passed.
 
         """
-
+        self.cache = FileCache('snowmobile', flag='cs')
         self.config_file = config_file
         self.conn_name = conn_name
+        self.path_to_config = self.cache.get(r'path_to_config')
+
+    def clear_cache(self) -> object:
+        """Clears cached path to credentials file."""
+        self.cache.clear()
+        return self
+
+    def cache_exists(self) -> bool:
+        """Checks to see if a cached file path exists to a valid file."""
+        try:
+            return os.path.isfile(self.path_to_config)
+        except:
+            return False
+
+    def cache_valid_for_config(self) -> bool:
+        """Checks to see if the valid file path contains the config file."""
+        try:
+            return self.config_file == os.path.basename(self.path_to_config)
+        except:
+            return False
+
+    def locate_config(self):
+        """Find config file by traversing file system from bottom up."""
+        for dirpath, dirnames, files in os.walk(os.path.expanduser('~'),
+                                                topdown=False):
+            if self.config_file in files:
+
+                self.path_to_config = os.path.join(dirpath, self.config_file)
+                break
+
+            else:
+                self.path_to_config = ''
+
+        return self.path_to_config
+
+    def get_creds_path(self) -> str:
+        """Checks for cache existence and validates - traverses OS if not.
+        """
+        print("Locating credentials...")
+
+        print("\t<1 of 2> Checking for cached path...")
+        if self.cache_exists() and self.cache_valid_for_config():
+
+            print(f"\t<2 of 2> Found cached path: {self.path_to_config}")
+
+        else:
+
+            print("\t<2 of 2> Cached path not found")
+            print(f"\nLooking for {self.config_file} in local file system..")
+
+            self.path_to_config = \
+                self.locate_config()
+
+            if self.path_to_config:
+                print(f"\t<1 of 1> '{self.config_file}' found at: "
+                      f"{self.path_to_config}")
+            else:
+                print(f"\t<1 of 1> Could not find config file"
+                      f" {self.config_file} please double check the name of "
+                      f"your configuration file or value passed in the"
+                      f"'config_file' argument")
+
+        return self.path_to_config
+
+    @staticmethod
+    def get_conn(all_creds: dict, conn_name: str, config_file: str):
+        """Finds default connection in config file."
+
+        Args:
+            all_creds: Dictionary of all credentials
+            conn_name: Name of connection (if passed)
+            config_file: Name of config file (for console output only)
+
+        Returns:
+            Name of the set of credentials to be parsed out of the
+            credentials file
+
+        """
+        print("\nImporting credentials...")
+        if not conn_name:
+            conn_name = next(iter(all_creds.keys()))
+            print(f"\t<1 of 2> No explicit connection passed, fetching "
+                  f"'{conn_name}' credentials by default")
+
+        else:
+            print(f"\t<1 of 2> Fetching user-specific 'conn_name={conn_name}' "
+                  f"from {config_file}")
+
+        return conn_name
 
     def get(self) -> dict:
         """Locates creds file and parses out the specified set of credentials.
@@ -26,49 +117,30 @@ class Credentials:
             Dictionary containing a specific set of Snowflake credentials
 
         """
-        print("Locating & importing credentials..")
-        print(f"\t<1 of 4> Searching for {self.config_file} in local file "
-              f"system..")
-        for dirpath, dirnames, files in os.walk(os.path.expanduser('~'),
-                                                topdown=False):
-            if self.config_file in files:
-                path_to_config = os.path.join(dirpath, self.config_file)
-                break
-            else:
-                path_to_config = ''
-                pass
+        self.path_to_config = self.get_creds_path()
+        self.cache['path_to_config'] = self.path_to_config
 
-        if not os.path.isfile(path_to_config):
-            print(f"\tCould not find {self.config_file} in file system!")
-        else:
-            with open(path_to_config) as c:
+        try:
+            with open(self.path_to_config) as c:
                 all_creds = json.load(c)
-            print(f"\n\t<2 of 4> Located & loaded {self.config_file} "
-                  f"from:\n\t\t{path_to_config}")
 
-        if not self.conn_name:
-            self.conn_name = next(iter(all_creds.keys()))
-            print(f"\n\t<3 of 4> No explicit connection passed, fetching "
-                  f"'{self.conn_name}' credentials by default")
-        else:
-            print(f"\n\t<3 of 4> Fetching '{self.conn_name}' credentials "
-                  f"from {self.config_file}")
-            # pass
+        except IOError as e:
+            print(e)
 
-        creds = all_creds.get(self.conn_name)
+        try:
+            self.conn_name = self.get_conn(all_creds, self.conn_name,
+                                           self.config_file)
+            creds = all_creds.get(self.conn_name.lower())
+            print(f"\t<2 of 2> Successfully imported credentials for "
+                  f"'{self.conn_name}'")
+            return creds
 
-        if creds:
-            print(f"\n\t<4 of 4> Successfully imported credentials for "
-                  f"conn_name='{self.conn_name}'")
-
-        else:
-            print(f"\n\t<4 of 4> Could not parse conn_name='{self.conn_name}'"
-                  f"from {self.config_file}\n\t\tPlease specify a different "
-                  f"configuration file, connection name, or check that the "
-                  f"contents of the configuration file")
-
-        return creds
-
-# creds = Credentials().get()
-# creds.get()
-# creds.conn_name
+        except IOError as e:
+            print(
+                f"\t<2 of 2>Could not parse conn_name='{self.conn_name}' "
+                f"from {self.config_file}\nPlease either "
+                f"\n\t\t- Specify a different configuration file or "
+                f"connection name"
+                f"\n\t\t- Verify the contents of the configuration file"
+                f"\n\t\t- Run snowmobile.snowcreds.cache.clear()")
+            print(e)
